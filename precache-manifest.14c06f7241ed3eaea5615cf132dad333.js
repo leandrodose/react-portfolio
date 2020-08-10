46d97121e7979a65043ee18a240802083c17ea1a
leandrodose <69464418+leandrodose@users.noreply.github.com>
2020-08-10 14:09:41 -0300

    Updates

---
+++ precache-manifest.14c06f7241ed3eaea5615cf132dad333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190bf869aa15ce0e46f097a5cbec429d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ded64058fd0c40bf0354",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d265d86b8b25b7734432",
+    "url": "/react-portfolio/static/js/2.09577e97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.09577e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded64058fd0c40bf0354",
+    "url": "/react-portfolio/static/js/main.25101301.chunk.js"
+  },
+  {
+    "revision": "c37395a2012e0505e8b4",
+    "url": "/react-portfolio/static/js/runtime-main.a9c1da9a.js"
+  }
+]);
